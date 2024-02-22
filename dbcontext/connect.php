0781a5f75c9d37db52eedcc5594431bc5db781f4
Mahmoud Gamal <mgamal@sgs-uae.com>
2024-02-22 14:53:43 +0200

    db

--- dbcontext/connect.php
+++ dbcontext/connect.php
@@ -1,11 +1,13 @@
 <?php
+
+require __DIR__. '/../utils/config.php';
 $DB_HOST = "localhost";
 $DB_USER = "mestamal_root";
 $DB_PASSWORD = "AU+PFln,auzD";
 $DB_DATABASE = "mestamal_new";
 
 // Create a connection
-$con = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
+$con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 // Check the connection
 if ($con->connect_error) {
--- utils/config.php
+++ utils/config.php
@@ -1,23 +1,25 @@
 <?php
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_DATABASE', 'most3mal');
-// define('CONNECTION_STRING','mysql:host=localhost;dbname=most3mal');
-// define('API_KEY', 'your_api_key');
-// define('API_URL', 'http://localhost/most3mal/api/dynamic/api.php/');
-// define('ROOT_URL', 'http://localhost/most3mal/');
-// define('DEBUG_MODE', true);
-// define('DSA', true);
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'mestamal_root');
-define('DB_PASSWORD', 'AU+PFln,auzD');
-define('DB_DATABASE', 'mestamal_new');
-define('CONNECTION_STRING', 'mysql:host=localhost;dbname=mestamal_new');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'most3mal');
+define('CONNECTION_STRING','mysql:host=localhost;dbname=most3mal');
 define('API_KEY', 'your_api_key');
-define('API_URL', 'https://mestamal.com/dashboard/api/dynamic/api.php/');
-define('ROOT_URL', 'https://mestamal.com/');
-define('SIGNIN_URL', 'https://mestamal.com/signin.php');
+define('API_URL', 'http://localhost/most3mal/api/dynamic/api.php/');
+define('ROOT_URL', 'http://localhost/most3mal/');
+define('SIGNIN_URL', 'http://localhost/most3mal/signin.php');
 define('DEBUG_MODE', true);
 define('DSA', true);
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'mestamal_root');
+// define('DB_PASSWORD', 'AU+PFln,auzD');
+// define('DB_DATABASE', 'mestamal_new');
+// define('CONNECTION_STRING', 'mysql:host=localhost;dbname=mestamal_new');
+// define('API_KEY', 'your_api_key');
+// define('API_URL', 'https://mestamal.com/dashboard/api/dynamic/api.php/');
+// define('ROOT_URL', 'https://mestamal.com/');
+// define('SIGNIN_URL', 'https://mestamal.com/signin.php');
+// define('DEBUG_MODE', flase);
+// define('DSA', true);
